54670e10625df3bb1c26c2b4510606d7f975f990
HaakonSkov <130569276+HaakonSkov@users.noreply.github.com>
2024-01-24 11:16:25 +0100

    Work on num

---
+++ VR - Escaperoom/Assets/Scripts/NumPadManeger.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumPadManeger : MonoBehaviour
+{
+   
+    public string trueCode;
+    List<string> code = new List<string>();
+    public TextMeshProUGUI codeDesplay;
+
+    void Start()
+    {
+        
+    }
+
+    public void ButtonInput(string i)
+    {
+        if (i == "C")
+        {
+            code = new List<string>();
+            codeDesplay.text = "";
+        }
+        if (i == "E")
+        {
+
+        }
+        else if (code.Count > 4)
+        {
+            code.Add(i);
+            codeDesplay.text = string.Join(' ', code);
+        }
+
+
+    }
+
+
+    
+    
+}
