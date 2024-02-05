23c69d2c36051543936bf72df5ea0975b697333b
Mads042p <61921471+Magsigmus@users.noreply.github.com>
2024-02-05 13:04:43 +0100

    Worked on gameManager

--- VR - Escaperoom/Assets/GameManagerBehaviour.cs
+++ VR - Escaperoom/Assets/GameManagerBehaviour.cs
@@ -2,16 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.SceneManagement;
 
 public class PrimaryButtonEvent : UnityEngine.Events.UnityEvent<bool> { }
 
 public class GameManagerBehaviour : MonoBehaviour
 {
     public UnityEngine.InputSystem.InputActionReference restartInput;
-    private bool lastButtonState = false;
+    private bool startedRestarting = false;
+    private float timeSinceButtonDown = 0;
+    public float timeToTriggerRestart = 1; 
 
     private void Update()
     {
+        restartInput.action.started += StartedRestart;
+        restartInput.action.canceled += CanceledRestart;
 
+        if (startedRestarting)
+        {
+            timeSinceButtonDown += Time.deltaTime;
+            if (timeSinceButtonDown >= timeToTriggerRestart) 
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+            }
+        }
+
+        startedRestarting = restartInput.action.triggered;
+    }
+
+    private void StartedRestart(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        Debug.Log("Button Down!");
+        startedRestarting = true;
+        timeSinceButtonDown = 0;
+    }
+
+    private void CanceledRestart(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        startedRestarting = false;
     }
 }
