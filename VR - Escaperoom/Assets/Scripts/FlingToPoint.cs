577d106f38c68a771d026f91d11d1a1874aac0fb
HaakonSkov <130569276+HaakonSkov@users.noreply.github.com>
2024-02-06 11:06:09 +0100

    Prancipet ferdi med glow til telekenises

--- VR - Escaperoom/Assets/Scripts/FlingToPoint.cs
+++ VR - Escaperoom/Assets/Scripts/FlingToPoint.cs
@@ -6,34 +6,81 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class FlingToPoint : MonoBehaviour
 {
-    public float forceMultiplyer = 1f;
+    public float forceMultiplyer = 2f;
+    public float gravityAdder = 3f;
     public bool grabing = false;
     public InputActionReference trigger;
-    public float gravityAdder = 5f;
     public GameObject glowObject;
+    public GameObject lightObj;
+    public Color hoverColor;
+    public Color celectedColor;
+    public Color readyColor;
+    public float startIntens = 1f;
+    public float endIntens = 5f;
+    public float neededSpeedMultyplier = 1f;
+    Light light;
     GameObject objToBeFling;
+    Rigidbody flingRb;
     Rigidbody rb;
-
+    bool celect = false;
+    Vector3 neededSpeed;
+    Vector3 lastCord;
+    Vector3 curentSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-           
+        rb = this.GetComponent<Rigidbody>();
+        light = lightObj.GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!grabing && Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+        if (!grabing && Physics.Raycast(transform.position, transform.forward, out RaycastHit hit) && !celect)
         {
             Debug.DrawRay(transform.position, transform.forward);
 
             objToBeFling = hit.transform.gameObject;
-            rb = objToBeFling.GetComponent<Rigidbody>();
-            if (objToBeFling.GetComponent<XRGrabInteractable>() != null && rb != null && trigger.action.triggered)
+            flingRb = objToBeFling.GetComponent<Rigidbody>();
+
+            if (objToBeFling.GetComponent<XRGrabInteractable>() != null && flingRb != null)
             {
-                rb.velocity = new Vector3(ForceToThis("x", objToBeFling), ForceToThis("y", objToBeFling), ForceToThis("z", objToBeFling));
+                light.enabled = true;
+                light.color = hoverColor;
+                light.intensity = startIntens;
+                lightObj.transform.position = objToBeFling.transform.position;
+                if (trigger.action.triggered)
+                {
+                    if (grabing) { light.enabled = false; return; }
+                    celect = true;
+                    neededSpeed = (objToBeFling.transform.position - this.transform.position) / 1000 * neededSpeedMultyplier;
+                    lastCord = transform.position;
+                }
             }
+        }
+        else if (celect)
+        {
+            lightObj.transform.position = objToBeFling.transform.position;
+            curentSpeed = transform.position - lastCord;
+
+            if (signCheck())
+            {
+                light.intensity = startIntens + IntenetyCordCalc("x") + IntenetyCordCalc("y") + IntenetyCordCalc("z");
+                if (trigger.action.phase == InputActionPhase.Waiting && speedCheck())
+                {
+                    flingRb.velocity = new Vector3(ForceToThis("x", objToBeFling), ForceToThis("y", objToBeFling), ForceToThis("z", objToBeFling));
+                }
+            }
+            if (trigger.action.phase == InputActionPhase.Waiting)
+            {
+                celect = false;
+            }
+            lastCord = transform.position;
+        }
+        else
+        {
+            light.enabled = false;
         }
     }
 
@@ -54,6 +101,34 @@ public class FlingToPoint : MonoBehaviour
         }
         return 12f;
     }
+    float IntenetyCordCalc(string axis)
+    {
+        if (axis == "x")
+        {
+            return (Mathf.Abs(transform.position.x) - Mathf.Abs(lastCord.x) / neededSpeed.x * (endIntens - startIntens));
+        }
+        else if (axis == "y")
+        {
+            return (Mathf.Abs(transform.position.y) - Mathf.Abs(lastCord.y) / neededSpeed.y * (endIntens - startIntens));
+        }
+        else
+        {
+            return (Mathf.Abs(transform.position.z) - Mathf.Abs(lastCord.z) / neededSpeed.z * (endIntens - startIntens));
+        }
+    }
+    bool signCheck()
+    {
+            return Mathf.Sign(transform.position.x - lastCord.x) == Mathf.Sign(neededSpeed.x) &&
+            Mathf.Sign(transform.position.y - lastCord.y) == Mathf.Sign(neededSpeed.y) &&
+            Mathf.Sign(transform.position.z - lastCord.z) == Mathf.Sign(neededSpeed.z);
+    }
+    bool speedCheck()
+    {
+        return Mathf.Abs(curentSpeed.x) > Mathf.Abs(neededSpeed.x)
+        && Mathf.Abs(curentSpeed.x) > Mathf.Abs(neededSpeed.x)
+        && Mathf.Abs(curentSpeed.x) > Mathf.Abs(neededSpeed.x);
+    }
+
 
     public void grab() { grabing = true; }
     public void release() { grabing = false; }
